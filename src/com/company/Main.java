3b2e47dfd33de8b496d4a77b336b554e7350c3ce
Belinda chanda <btatend1@stu.vistula.edu.pl>
2022-05-28 16:13:54 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) { {
+        int sum=0;
+        for(int i=1;i<100;i++)
+        {
+            if(i%2==0)
+            {
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+    }
+    }
+	// write your code here
+    }
+}
